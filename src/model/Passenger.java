62c65d03813ff67f7dd77f03ef48537c1200126c
ziiihooo <1045406918@qq.com>
2022-03-28 16:29:51 +0800

    just try github desktop

--- src/model/Passenger.java
+++ src/model/Passenger.java
@@ -5,6 +5,10 @@ package model;
  * @description: User model
  * @date 2022/3/16 16:27
  */
+
+/**
+ * just try to use github desktop (yzh)
+ */
 public class Passenger {
     private Integer bookingNumber;//与航班挂钩
     private Integer flightId;
