80c2eaf96b4fe169bca32732e1a4fdafde14b2ca
Yichen Liu <jp2019213039@qmul.ac.uk>
2022-05-19 17:32:03 +0800

    数据更新已找到问题，下一步：一键更新+toJSON补全

--- src/test/java/mock/MockAll.java
+++ src/test/java/mock/MockAll.java
@@ -1,6 +1,9 @@
 package mock;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.io.IOException;
 
 /**
  * @author YichenLiu
@@ -9,4 +12,32 @@ import junit.framework.TestCase;
  */
 public class MockAll extends TestCase {
 
+    @Test
+    public void refreshData(){
+        PassengerMock passengerMock = new PassengerMock();
+        IdDocumentCardMock idDocumentCardMock = new IdDocumentCardMock();
+        FlightSeatMock flightSeatMock = new FlightSeatMock();
+        FlightMock flightMock = new FlightMock();
+        AirlineMock airlineMock = new AirlineMock();
+        AirlineMealMock airlineMealMock = new AirlineMealMock();
+        try {
+            //passenger
+            passengerMock.toJSON(null,true);
+            //idDocumentCard
+            idDocumentCardMock.toJSON(1111,"Zihao Ye");
+            idDocumentCardMock.toJSON(2222,"Yichen Liu");
+            idDocumentCardMock.toJSON(3333,"Ran Xu");
+            idDocumentCardMock.toJSON(4444,"Zhihan Zhang");
+            //seat
+            flightSeatMock.toJSON(null,true);
+            //flight
+            flightMock.toJSON(null, true);
+            //airline
+            airlineMock.toJSON(null,true);
+            //meal
+            airlineMealMock.toJSON(null,true);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
