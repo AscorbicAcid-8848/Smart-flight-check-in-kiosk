9c630df25925f08a7ef484caedad961ef8fad125
RanXu2001 <1920784565@qq.com>
2022-05-23 17:08:07 +0800

    1

--- src/main/java/boundary/Entrance.java
+++ src/main/java/boundary/Entrance.java
@@ -1,7 +1,24 @@
 package boundary;
 
+import com.jtattoo.plaf.mint.MintLookAndFeel;
+
+import javax.swing.*;
+
 public class Entrance {
     public static void main(String args[]){
+        Entrance entrance = new Entrance();
+        entrance.initialize();
+    }
+
+    private void initialize() {
+
+        try {
+            UIManager.setLookAndFeel(new MintLookAndFeel());
+
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
+        }
         InitPage initPage = new InitPage();
     }
+
 }
